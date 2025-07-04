2fcbde23918a3b9f3f6fc0fab4525a9810f936c1
Yuyi-hao <roshanisingh2354@gmail.com>
2025-07-04 20:30:44 +0530

    [FIX] added error handling

--- audioCNN_api.py
+++ audioCNN_api.py
@@ -4,6 +4,8 @@ from flask_cors import CORS
 import json
 import os
 from dotenv import load_dotenv
+import traceback
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -12,12 +14,21 @@ CORS(app, origins=[os.getenv("FRONTEND_URL")])
 
 @app.route("/predict/", methods=['POST'])
 def predict_sound():
-    data = request.data
-    data = data.decode('utf-8')
-    data = json.loads(data)
-    audio_data = data['audio_data']
-    result = predict_audio_from_model(audio_data)
-    return jsonify(result)
+    try:
+        data = request.data
+        data = data.decode('utf-8')
+        data = json.loads(data)
+        audio_data = data['audio_data']
+        if not audio_data:
+            return jsonify({"error": "Missing 'audio_data' in request"}), 400
+        
+        result = predict_audio_from_model(audio_data)
+        return jsonify(result)
+    
+    except Exception as e:
+        traceback.print_exc()  # logs error in server logs
+        return jsonify({"error": "Internal Server Error", "details": str(e)}), 500
+        
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000, debug=os.getenv("DEBUG")=="True")
